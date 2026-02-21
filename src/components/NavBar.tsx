fbb0b9d9ca789c3afa858b7154334e68a1c97d9d
harshith reddy chiluka <harshithkalki@gmail.com>
2026-02-21 12:08:21 -0500

    init and nav

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,46 @@
+const navItems = [
+  {
+    id: "about",
+    label: "About",
+  },
+  {
+    id: "skills",
+    label: "Skills",
+  },
+  {
+    id: "projects",
+    label: "Projects",
+  },
+];
+
+function scrollToSection(id: string) {
+  const el = document.getElementById(id);
+  if (!el) return;
+  el.scrollIntoView({ behavior: "smooth", block: "start" });
+}
+
+const NavBar = () => {
+  return (
+    <>
+      <header className="sticky top-0 z-50 bg-[rgb(9,14,26)] opacity-70">
+        <div className="py-6 px-8 mx-auto justify-around items-center flex">
+          <div className="font-mono  text-[#3de0d0] text-lg">{"<dev />"}</div>
+          <nav className="hidden md:flex items-center gap-6">
+            {navItems.map((item, index) => (
+              <button
+                key={item.id}
+                className="font-mono hover:text-[#3de0d0] transition text-[#7689a3] text-xs"
+                onClick={() => scrollToSection(item.id)}
+              >
+                <span className="text-[#3de0d0]">0{index + 1}. </span>
+                {item.label}
+              </button>
+            ))}
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default NavBar;
