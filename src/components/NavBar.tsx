99dbbb8b04cca59be94667ac6c31430ca4558517
harshith reddy chiluka <harshithkalki@gmail.com>
2026-02-26 16:03:26 -0500

    page layout changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -22,21 +22,23 @@ function scrollToSection(id: string) {
 const NavBar = () => {
   return (
     <>
-      <header className="sticky top-0 z-50 bg-[rgb(9,14,26)] opacity-70">
-        <div className="py-6 px-8 mx-auto justify-around items-center flex">
-          <div className="font-mono  text-[#3de0d0] text-lg">{"<dev />"}</div>
-          <nav className="hidden md:flex items-center gap-6">
-            {navItems.map((item, index) => (
-              <button
-                key={item.id}
-                className="font-mono hover:text-[#3de0d0] transition text-[#7689a3] text-xs"
-                onClick={() => scrollToSection(item.id)}
-              >
-                <span className="text-[#3de0d0]">0{index + 1}. </span>
-                {item.label}
-              </button>
-            ))}
-          </nav>
+      <header className="sticky top-0 z-50 bg-[rgb(9,14,26)] opacity-70 ">
+        <div className="max-w-7xl mx-auto">
+          <div className="py-6  justify-between items-center flex">
+            <div className="font-mono  text-[#3de0d0] text-lg">{"<dev />"}</div>
+            <nav className="hidden md:flex items-center gap-6">
+              {navItems.map((item, index) => (
+                <button
+                  key={item.id}
+                  className="font-mono hover:text-[#3de0d0] transition text-[#7689a3] text-sm"
+                  onClick={() => scrollToSection(item.id)}
+                >
+                  <span className="text-[#3de0d0]">0{index + 1}. </span>
+                  {item.label}
+                </button>
+              ))}
+            </nav>
+          </div>
         </div>
       </header>
     </>
