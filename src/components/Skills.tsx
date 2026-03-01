c727649c85ae5a06a768c7a0d12736a77835b933
harshith reddy chiluka <harshithkalki@gmail.com>
2026-03-01 13:39:04 -0500

    skills layout changes to grid layout v2

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -23,50 +23,20 @@ const Skills = () => {
       <pre className="text-2xl font-semibold">
         <span className="font-normal text-[#3de0d0]">02. </span>Tech Stack
       </pre>
-      <div className="flex flex-col md:flex-row justify-around md:justify-between items-center mt-8">
-        <div className="w-[80vw] md:w-55 md:h-full p-4 bg-[#0b111f] border border-[#161e2e] hover:border-[#3de0d0] mt-10 rounded-lg">
-          <h3 className="text-lg font-medium text-[#3de0d0] mb-4">
-            {techStack[0].name}
-          </h3>
-          <ul className="list-disc list-inside text-[#7487a3]">
-            {techStack[0].techs.map((tech) => (
-              <li key={tech}>{tech}</li>
-            ))}
-          </ul>
-        </div>
 
-        <div className="w-[80vw] md:w-55 md:h-full  p-4 bg-[#0b111f] border border-[#161e2e] hover:border-[#3de0d0] mt-10 rounded-lg">
-          <h3 className="text-lg font-medium text-[#3de0d0] mb-4">
-            {techStack[1].name}
-          </h3>
-          <ul className="list-disc list-inside text-[#7487a3]">
-            {techStack[1].techs.map((tech) => (
-              <li key={tech}>{tech}</li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="w-[80vw] md:w-55 md:min-h-full  p-4 bg-[#0b111f] border border-[#161e2e] hover:border-[#3de0d0] mt-10 rounded-lg">
-          <h3 className="text-lg font-medium text-[#3de0d0] mb-4">
-            {techStack[2].name}
-          </h3>
-          <ul className="list-disc list-inside text-[#7487a3]">
-            {techStack[2].techs.map((tech) => (
-              <li key={tech}>{tech}</li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="w-[80vw] md:w-55  p-4 bg-[#0b111f] border border-[#161e2e] hover:border-[#3de0d0] mt-10 rounded-lg">
-          <h3 className="text-lg font-medium text-[#3de0d0] mb-4">
-            {techStack[3].name}
-          </h3>
-          <ul className="list-disc list-inside text-[#7487a3]">
-            {techStack[3].techs.map((tech) => (
-              <li key={tech}>{tech}</li>
-            ))}
-          </ul>
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-8">
+        {techStack.map((stack) => (
+          <div className="w-[80vw] md:w-55 md:h-full p-6 bg-[#0b111f] border border-[#161e2e] hover:border-[#3de0d0] mt-10 rounded-lg">
+            <h3 className="text-lg font-medium text-[#3de0d0] mb-4">
+              {stack.name}
+            </h3>
+            <ul className="list-disc list-inside text-[#7487a3]">
+              {stack.techs.map((tech) => (
+                <li key={tech}>{tech}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </section>
   );
